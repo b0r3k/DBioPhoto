4439ede6a2e38c7b8bd3d3d738290745e289ff4e
Borek Požár <pozar.borek@gmail.com>
2020-09-26 15:50:43 +0200

    Added class and functions for other filters. TODO BlossomColour joining tables.

---
+++ DBioPhoto.DataAccess/Services/Filtering/BasedOnOther.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using DBioPhoto.Domain.Models;
+using DBioPhoto.DataAccess.Data;
+
+namespace DBioPhoto.DataAccess.Services.Filtering
+{
+    class BasedOnOther
+    {
+        private readonly DBioPhotoContext _context;
+
+        public BasedOnOther(DBioPhotoContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Photo>> GetWithCategory(Category category)
+        {
+            List<Photo> photos = await (
+                from p in _context.Photos.AsNoTracking()
+                where (p.Category == category)
+                orderby p.TimeCreated descending
+                select p
+                ).ToListAsync();
+            return photos;
+        }
+
+        public async Task<List<Photo>> GetWithBlossomColour(BlossomColour blossomColour)
+        {
+            /*List<Photo> photos = await (
+                from p in _context.Photos.AsNoTracking()
+                where (from plant in _context.Plants.AsNoTracking()
+                       (where plant.BlossomColour == blossomColour)
+                       select plant)
+                orderby p.TimeCreated descending
+                select p
+                ).ToListAsync();*/
+            List<Photo> photos = await (            // NOT WORKING!
+                from p in _context.Photos.AsNoTracking()
+                select p
+                ).ToListAsync();
+            return photos;
+        }
+
+        public async Task<List<Photo>> GetWithCommentContains(string comment)
+        {
+            List<Photo> photos = await (
+                from p in _context.Photos.AsNoTracking()
+                where (p.Comment.Contains(comment))
+                orderby p.TimeCreated descending
+                select p
+                ).ToListAsync();
+            return photos;
+        }
+    }
+}
