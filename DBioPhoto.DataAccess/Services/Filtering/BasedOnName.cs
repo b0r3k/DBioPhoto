7696f93ec797e665cd8397e28c6f9e03067cbbce
Borek Požár <pozar.borek@gmail.com>
2020-09-26 15:22:51 +0200

    Added class to filter based on Name. Function to filter based on CzFirstName. TODO joining tables.

---
+++ DBioPhoto.DataAccess/Services/Filtering/BasedOnName.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using DBioPhoto.Domain.Models;
+using DBioPhoto.DataAccess.Data;
+
+namespace DBioPhoto.DataAccess.Services.Filtering
+{
+    class BasedOnName
+    {
+        private readonly DBioPhotoContext _context;
+
+        public BasedOnName(DBioPhotoContext context)
+        {
+            _context = context;
+        }
+
+        // Case sensitive?
+        public async Task<List<Photo>> GetWithCatAndCzFirstName(Category category, string name)
+        {
+            List<Photo> photos = await (
+                from p in _context.Photos.AsNoTracking()
+                where (p.Category == category) // Need to find out how to "join" tables and search based on name
+                orderby p.TimeCreated descending
+                select p
+                ).ToListAsync();
+            return photos;
+        }
+    }
+}
