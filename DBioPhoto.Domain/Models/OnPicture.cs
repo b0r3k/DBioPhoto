1bb47bf34fc566f4486b931f1ba14f5cf104fab5
Borek Požár <pozar.borek@gmail.com>
2021-03-02 10:25:13 +0100

    Added Nickname option for Person OnPhoto.

--- DBioPhoto.Domain/Models/OnPicture.cs
+++ DBioPhoto.Domain/Models/OnPicture.cs
@@ -45,6 +45,7 @@ namespace DBioPhoto.Domain.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Nickname { get; set; }
         public string Surname { get; set; }
 
         public ICollection<Photo> Photos { get; set; }
