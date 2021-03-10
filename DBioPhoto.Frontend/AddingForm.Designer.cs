a7a48f3222dff0c7b709d44195ca139f5c32a97b
Borek Požár <pozar.borek@gmail.com>
2021-03-10 15:43:33 +0100

    AddingForm | Folder choosing working.

--- DBioPhoto.Frontend/AddingForm.Designer.cs
+++ DBioPhoto.Frontend/AddingForm.Designer.cs
@@ -30,24 +30,56 @@ namespace DBioPhoto.Frontend
         private void InitializeComponent()
         {
             this.organismAddButton = new System.Windows.Forms.Button();
+            this.folderPathTextBox = new System.Windows.Forms.TextBox();
+            this.folderChooseButton = new System.Windows.Forms.Button();
+            this.folderChooseLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // organismAddButton
             // 
             this.organismAddButton.Anchor = System.Windows.Forms.AnchorStyles.Right;
-            this.organismAddButton.Location = new System.Drawing.Point(833, 64);
+            this.organismAddButton.Location = new System.Drawing.Point(1370, 50);
             this.organismAddButton.Name = "organismAddButton";
-            this.organismAddButton.Size = new System.Drawing.Size(125, 23);
+            this.organismAddButton.Size = new System.Drawing.Size(119, 23);
             this.organismAddButton.TabIndex = 0;
             this.organismAddButton.Text = "Přidávat organismy";
             this.organismAddButton.UseVisualStyleBackColor = true;
             this.organismAddButton.Click += new System.EventHandler(this.organismAddButton_Click);
+            // 
+            // folderPathTextBox
+            // 
+            this.folderPathTextBox.Location = new System.Drawing.Point(32, 38);
+            this.folderPathTextBox.Name = "folderPathTextBox";
+            this.folderPathTextBox.Size = new System.Drawing.Size(599, 23);
+            this.folderPathTextBox.TabIndex = 1;
+            this.folderPathTextBox.MouseClick += new System.Windows.Forms.MouseEventHandler(this.folderPathTextBox_MouseClick);
+            // 
+            // folderChooseButton
+            // 
+            this.folderChooseButton.Location = new System.Drawing.Point(649, 38);
+            this.folderChooseButton.Name = "folderChooseButton";
+            this.folderChooseButton.Size = new System.Drawing.Size(120, 23);
+            this.folderChooseButton.TabIndex = 2;
+            this.folderChooseButton.Text = "Zvolit složku";
+            this.folderChooseButton.UseVisualStyleBackColor = true;
+            // 
+            // folderChooseLabel
+            // 
+            this.folderChooseLabel.AutoSize = true;
+            this.folderChooseLabel.Location = new System.Drawing.Point(32, 20);
+            this.folderChooseLabel.Name = "folderChooseLabel";
+            this.folderChooseLabel.Size = new System.Drawing.Size(212, 15);
+            this.folderChooseLabel.TabIndex = 3;
+            this.folderChooseLabel.Text = "Zvolte složku, kterou chcete procházet:";
             // 
             // AddingForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1534, 1041);
+            this.Controls.Add(this.folderChooseLabel);
+            this.Controls.Add(this.folderChooseButton);
+            this.Controls.Add(this.folderPathTextBox);
             this.Controls.Add(this.organismAddButton);
             this.Name = "AddingForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
@@ -55,11 +87,15 @@ namespace DBioPhoto.Frontend
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.AddingForm_FormClosing);
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.AddingForm_FormClosed);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button organismAddButton;
+        private System.Windows.Forms.TextBox folderPathTextBox;
+        private System.Windows.Forms.Button folderChooseButton;
+        private System.Windows.Forms.Label folderChooseLabel;
     }
 }
--- DBioPhoto.Frontend/AddingForm.cs
+++ DBioPhoto.Frontend/AddingForm.cs
@@ -1,9 +1,11 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Windows.Forms;
 
 namespace DBioPhoto.Frontend
 {
     public partial class AddingForm : Form
     {
+        private string _folderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
         public AddingForm()
         {
             InitializeComponent();
@@ -24,6 +26,20 @@ namespace DBioPhoto.Frontend
         {
             OrganismAddForm newGui = new OrganismAddForm();
             newGui.Visible = true;
+        }
+
+        private void folderPathTextBox_MouseClick(object sender, MouseEventArgs e)
+        {
+            using (FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog())
+            {
+                folderBrowserDialog.RootFolder = Environment.SpecialFolder.MyDocuments;
+                if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of selected folder
+                    _folderPath = folderBrowserDialog.SelectedPath;
+                    folderPathTextBox.Text = _folderPath;
+                }
+            }
         }
     }
 }
