9ae2b6bcb774906291233eb4ca4cf99c9965faee
Borek Požár <pozar.borek@gmail.com>
2021-03-11 14:26:29 +0100

    AddPhoto | Backend for adding/updating photo to/in the db.

---
+++ DBioPhoto.DataAccess/Services/Adding/AddPhoto.cs
@@ -0,0 +1,34 @@
+﻿using DBioPhoto.DataAccess.Data;
+using DBioPhoto.Domain.Models;
+using System.Linq;
+
+namespace DBioPhoto.DataAccess.Services.Adding
+{
+    public class AddPhoto
+    {
+        public static string TryAddPhoto(DBioPhotoContext dbContext, Photo tryPhoto)
+        {
+            // No photo selected
+            if (tryPhoto.FileName == "" || tryPhoto.FilePath == "")
+                return "Zvolte fotku";
+
+            // Photo already in db, update
+            else if (dbContext.Photos.Any(p => p.FilePath == tryPhoto.FilePath))
+            {
+                Photo existingPhoto = dbContext.Photos.FirstOrDefault(p => p.FilePath == tryPhoto.FilePath);
+                existingPhoto.Category = tryPhoto.Category;
+                existingPhoto.Comment = tryPhoto.Comment;
+                existingPhoto.Location = tryPhoto.Location;
+                return "Úspěšně aktualizováno!";
+            }
+
+            // Add to db
+            else
+            {
+                dbContext.Photos.Add(tryPhoto);
+                dbContext.SaveChanges();
+                return "Úspěšně přidáno!";
+            }
+        }
+    }
+}
