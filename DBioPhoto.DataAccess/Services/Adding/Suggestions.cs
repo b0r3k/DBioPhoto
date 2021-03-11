d2012b779ac02fcddf3ce9d7d80758b3e4ae0112
Borek Požár <pozar.borek@gmail.com>
2021-03-11 15:02:23 +0100

    Suggestions | Backend for suggesting photo location and comment from db.

--- DBioPhoto.DataAccess/Services/Adding/Suggestions.cs
+++ DBioPhoto.DataAccess/Services/Adding/Suggestions.cs
@@ -24,5 +24,18 @@ namespace DBioPhoto.DataAccess.Services.Adding
                     return new string[0];
             }
         }
+
+        public static string[] GetPhotoInfoSuggestions(DBioPhotoContext dbContext, string beginning, int textBoxNumber)
+        {
+            switch (textBoxNumber)
+            {
+                case 0:
+                    return dbContext.Photos.Where(p => p.Location.StartsWith(beginning.ToLower())).Select(p => p.Location).Distinct().ToArray();
+                case 1:
+                    return dbContext.Photos.Where(p => p.Comment.StartsWith(beginning.ToLower())).Select(p => p.Comment).Distinct().ToArray();
+                default:
+                    return new string[0];
+            }
+        }
     }
 }
